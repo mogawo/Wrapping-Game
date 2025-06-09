1482e053555e883a50fcb59820c6eff9a4130a81
phuochl1 <phuochl1@uci.edu>
2025-06-08 22:52:35 -0700

    Initial Commit

---
+++ bullet.gd
@@ -0,0 +1,169 @@
+extends RigidBody3D
+
+@export var SPEED = 50
+@export var DRAW_LINE := true
+@export var COLOR: Color
+@export var REEL_STEP := 0.01
+@export var SLACK := 100
+
+var SHOOT = false
+
+var DRAW := false
+var ATTACHED := false
+
+var REEL_DIR := 0
+
+var STIFFNESS := 1000
+var REST_LENGTH := 0
+var DAMPING := 100
+
+var PLAYER: CharacterBody3D
+var TARGET: Node3D
+var ATTACH_POINT: Vector3
+
+@export var ROPE_GIRTH = 0.1
+
+func _ready() -> void:
+	self.set_as_top_level(true)
+	
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	if REEL_DIR:
+		REST_LENGTH = lerpf(REST_LENGTH, 0, REEL_STEP)
+	if DRAW and ATTACHED:
+		#update_line(ATTACH_POINT, TARGET.global_position, COLOR)
+		update_cast(ATTACH_POINT)
+		update_forward_cast()
+	if not ATTACHED:
+		ATTACH_POINT = global_position
+	if ATTACHED and PLAYER:
+		handle_grapple(delta)
+		
+
+func handle_grapple(delta: float) -> void:
+	var player := PLAYER
+	var target_dir = player.position.direction_to(position)
+	var target_dist = player.position.distance_to(position)
+	var displacement = target_dist - REST_LENGTH
+	
+	var force := Vector3.ZERO
+	if displacement > 0:
+		var spring_force_magnitude = STIFFNESS * displacement
+		var spring_force = target_dir * spring_force_magnitude
+		var vel_dot = player.velocity.dot(target_dir)
+		var vert_damping = -DAMPING * vel_dot * target_dir
+		var hori_damping = -player.velocity / 2
+		force = spring_force + vert_damping + hori_damping
+	player.velocity += force * delta
+
+func reel_in():
+	REEL_DIR = 1
+func reel_stop():
+	REEL_DIR = 0
+func reel_out():
+	REEL_DIR = -1
+func fire():
+	apply_central_impulse(-transform.basis.z * SPEED)
+	pass
+
+func attach_player(player: CharacterBody3D):
+	PLAYER = player
+
+func draw_line_to(target: Node3D, color := COLOR):
+	TARGET = target
+	DRAW = true
+	COLOR = color
+	#lwjnqdjnqwodnwjod
+
+func draw_contact_point(pos: Vector3, color: Color):
+	var waypoint = MeshInstance3D.new()
+	waypoint.set_as_top_level(true)
+	waypoint.name = "WAYPOINT"
+	add_child(waypoint)
+	waypoint.global_position = Vector3.ZERO
+	var material = ORMMaterial3D.new()
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = color
+	
+	waypoint.mesh = ImmediateMesh.new()
+	waypoint.mesh.surface_begin(Mesh.PrimitiveType.PRIMITIVE_LINES, material)
+	waypoint.mesh.surface_add_vertex(pos)
+	waypoint.mesh.surface_add_vertex(pos + Vector3.UP * 100)
+	waypoint.mesh.surface_end()
+
+func create_line(origin = null):
+	var rope = MeshInstance3D.new()
+	$RopeMesh.add_child(rope)
+	rope.mesh = ImmediateMesh.new()
+	if origin:
+		ATTACH_POINT = origin
+
+func update_line(pos1: Vector3, pos2: Vector3, color):
+	var parts_count = $RopeMesh.get_child_count()
+	var rope_part: Node3D
+	if parts_count > 0:
+		rope_part = $RopeMesh.get_child(parts_count-1)
+	if rope_part:
+		var material = ORMMaterial3D.new()
+		rope_part.mesh = ImmediateMesh.new()
+		rope_part.mesh.surface_begin(Mesh.PrimitiveType.PRIMITIVE_LINES, material)
+		rope_part.mesh.surface_add_vertex(pos1)
+		rope_part.mesh.surface_add_vertex(pos2)
+		rope_part.mesh.surface_end()
+		material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+		material.albedo_color = color
+	
+		
+func create_rope_cast(origin = null):
+	var rope_cast = RayCast3D.new()
+	rope_cast.top_level = true
+	$RopeCast.add_child(rope_cast)
+	rope_cast.hit_back_faces = true
+	rope_cast.hit_from_inside = false
+	rope_cast.set_collision_mask_value(1, false)
+	rope_cast.set_collision_mask_value(2, true)
+	rope_cast.set_collision_mask_value(3, true)
+	rope_cast.add_exception(self)
+	rope_cast.enabled = true
+	if origin:
+		rope_cast.global_position = origin
+		ATTACH_POINT = origin
+
+func update_forward_cast():
+	$ForwardCast.global_position = TARGET.global_position
+	$ForwardCast.target_position = ATTACH_POINT - TARGET.global_position
+
+func update_cast(pos: Vector3):
+	var casts_count = $RopeCast.get_child_count()
+	var cast_part: RayCast3D
+	if casts_count > 0:
+		cast_part = $RopeCast.get_child(casts_count-1)
+		cast_part.global_position = ATTACH_POINT
+	if cast_part and cast_part.global_position != TARGET.global_position:
+		cast_part.target_position = TARGET.global_position - ATTACH_POINT
+	if cast_part and cast_part.is_colliding():
+		var contact_point = cast_part.get_collision_point()
+		var contact_normal_back = cast_part.get_collision_normal()
+		var contact_normal_forward = Vector3.ZERO
+		$ForwardCast.force_raycast_update()
+		if $ForwardCast.is_colliding():
+			contact_normal_forward = $ForwardCast.get_collision_normal()
+			draw_contact_point($ForwardCast.get_collision_point(), Color.BLUE)
+		contact_point += (contact_normal_back + contact_normal_forward) * 0.1
+		draw_contact_point(contact_point, Color.GREEN)
+		create_rope_cast(contact_point)
+		update_line(pos, contact_point, Color.AQUA)
+		create_line(contact_point)
+
+
+func _on_body_entered(body: Node) -> void:
+	if body is not CharacterBody3D:
+		self.linear_velocity = Vector3.ZERO
+		self.angular_velocity = Vector3.ZERO
+		REST_LENGTH = PLAYER.global_position.distance_to(global_position) + SLACK
+		ATTACHED = true
+		ATTACH_POINT = $RopeCast.global_position
+
+	
---
+++ player.gd
@@ -0,0 +1,87 @@
+extends CharacterBody3D
+
+
+@export var GROUND_SPEED = 5.0
+@export var AIR_SPEED = 10
+@export var JUMP_VELOCITY = 4.5
+@export_range(0.0, 1.0) var MOUSE_SENSITIITY = 0.002
+@export var TILT_LIMIT = deg_to_rad(75)
+@export var BULLET = preload("res://bullet.tscn")
+@export var MASS = 1
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		velocity.x = move_toward(velocity.x, direction.x * GROUND_SPEED, 10)
+		velocity.z = move_toward(velocity.z, direction.z * GROUND_SPEED, 10)
+	else:
+		velocity.x += direction.x * AIR_SPEED * delta
+		velocity.z += direction.z * AIR_SPEED * delta
+	
+	move_and_slide()
+	
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		if $CameraPivot/GunLeft/Gunpoint.get_node_or_null("Bullet"):
+			$CameraPivot/GunLeft/Gunpoint/Bullet.queue_free()
+		else:
+			spawn_bullet($CameraPivot/GunLeft/Gunpoint)
+			
+	if event.is_action_pressed("right_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		if $CameraPivot/GunRight/Gunpoint.get_node_or_null("Bullet"):
+			$CameraPivot/GunRight/Gunpoint/Bullet.queue_free()
+		else:
+			spawn_bullet($CameraPivot/GunRight/Gunpoint)
+
+	if event.is_action_pressed("reel_in_left"):
+		if $CameraPivot/GunLeft/Gunpoint.get_node_or_null("Bullet"):
+			$CameraPivot/GunLeft/Gunpoint/Bullet.reel_in()
+	if event.is_action_released("reel_in_left"):
+		if $CameraPivot/GunLeft/Gunpoint.get_node_or_null("Bullet"):
+			$CameraPivot/GunLeft/Gunpoint/Bullet.reel_stop()
+			
+	if event.is_action_pressed("reel_in_right"):
+		if $CameraPivot/GunRight/Gunpoint.get_node_or_null("Bullet"):
+			$CameraPivot/GunRight/Gunpoint/Bullet.reel_in()
+	if event.is_action_released("reel_in_right"):
+		if $CameraPivot/GunRight/Gunpoint.get_node_or_null("Bullet"):
+			$CameraPivot/GunRight/Gunpoint/Bullet.reel_stop()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		$CameraPivot.rotation.x -= event.relative.y * MOUSE_SENSITIITY
+		$CameraPivot.rotation.x = clampf($CameraPivot.rotation.x, -TILT_LIMIT, TILT_LIMIT)
+		self.rotation.y = clampf(self.rotation.y - event.relative.x * MOUSE_SENSITIITY, -TAU, TAU)
+
+func spawn_bullet(gunpoint: Node3D):
+	var bullet = BULLET.instantiate()
+	gunpoint.add_child(bullet)
+	bullet.attach_player(self)
+	bullet.fire()
+	bullet.create_line()
+	bullet.create_rope_cast()
+	bullet.draw_line_to(gunpoint)
+	
+	#print_tree_pretty()
+	
+		
