533aae5d1c13062827867ac15da0c39312cab7ff
phuochl1 <megagotnothing@gmail.com>
2025-06-20 21:04:46 -0700

    Refactor Physics Chain code into individual functions

--- chain.gd
+++ chain.gd
@@ -2,97 +2,65 @@
 extends Node3D
 class_name Chain
 
-@export var link = load("res://link.tscn")
+@export var link = preload("res://link.tscn")
 @export var seperation = 2
-@export_range(1, 10) var length = 1
+@export_range(0, 10) var length = 0
 
 var link_array: Array[Node]
 var joint_array: Array[PinJoint3D]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	#var current = get_child(0)
-	#for i in range(length):	
-		#var new_link: Link = link.instantiate()
-		#add_child(new_link)
-		#attach(current, new_link)
-		##print(i)
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var diff = link_array.size() - length
-	
-	#link_array.clear()
-	if diff < 0:
-		var new_link = link.instantiate()
-		var dir = link_array.front().transform.basis.y if not link_array.is_empty() else Vector3.UP
-		new_link.position = dir * link_array.size() * seperation
-		add_child(new_link)
-		link_array.append(new_link)
-		if link_array.size() > 1:
-			var new_joint := PinJoint3D.new()
-			print(typeof(new_joint.PARAM_BIAS))
-			new_joint.set_param(PinJoint3D.Param.PARAM_BIAS, 0.3)
-			new_joint.set_param(PinJoint3D.Param.PARAM_DAMPING, 1)
-			new_joint.set_param(PinJoint3D.Param.PARAM_IMPULSE_CLAMP, 0)
-			print(link_array.front().transform.basis.y)
-			var sep = 2 * (link_array.size()-2) + 1
-			new_joint.position = link_array.front().transform.basis.y * sep
-			new_joint.node_a = new_link.get_path()
-			new_joint.node_b = link_array[link_array.size() - 2].get_path()
-			add_child(new_joint)
-			
-			joint_array.append(new_joint)
-		pass
-	elif diff > 0:
-		link_array.pop_back().queue_free()
-		joint_array.pop_back().queue_free()
-	else:
-		#draw_contact_point(joint_array.back().global_position, Color.RED)
-		#get_children().pop_back().queue_free()
-		#link_array.pop_back().queue_free()
-		#link_array.clear()
-		#joint_array.pop_back().queue_free()
-		#get_children().pop_back().queue_free()
-		#for c in get_children():
-			#remove_child(c)
-			#c.queue_free()
-		#get_children().clear()
-		
-		pass
-	#print("Children: ", get_children())
-	#print("LinkArray: ", link_array)
-	#print(diff)
+	#clear_links()
+	#print_linkage()
+	handle_linkage()
 
-func _physics_process(delta: float) -> void:
-	attach_links()
-	pass
-	
-func attach_links():
-	var link_array = get_children()
-	for i in range(1, link_array.size() - 2):
-		var before = link_array[i-1]
-		var curr = link_array[i]
-		var after = link_array[i+1]
-		
+func print_linkage():
+	print("Links: ", link_array)
+	print("Joints: ", joint_array)
 
-func handle_linkage(delta: float):
-	pass
-	
-func draw_contact_point(pos: Vector3, color: Color):
-	var waypoint = MeshInstance3D.new()
-	waypoint.set_as_top_level(true)
-	waypoint.name = "WAYPOINT"
-	add_child(waypoint)
-	waypoint.global_position = Vector3.ZERO
-	var material = ORMMaterial3D.new()
-	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = color
-	
-	waypoint.mesh = ImmediateMesh.new()
-	waypoint.mesh.surface_begin(Mesh.PrimitiveType.PRIMITIVE_LINES, material)
-	waypoint.mesh.surface_add_vertex(pos)
-	waypoint.mesh.surface_add_vertex(pos + Vector3.UP * 100)
-	waypoint.mesh.surface_end()
+func handle_linkage():	
+	var diff = length - link_array.size()
+	if diff == 0: return
+	var link_call: Callable = add_link if diff > 0 else remove_link
+	link_call.call()
 
+func clear_links():
+	for l in link_array:
+		l.queue_free()
+	link_array.clear()
+	for j in joint_array:
+		j.queue_free()
+	joint_array.clear()
 	
+func remove_link():
+	print("remove_link")
+	if not link_array.is_empty(): link_array.pop_back().queue_free()
+	if not joint_array.is_empty(): joint_array.pop_back().queue_free()
+	
+func add_link():
+	print("add_link")
+	var new_link = link.instantiate()
+	var dir = link_array.front().transform.basis.y if not link_array.is_empty() else Vector3.UP
+	new_link.position = dir * link_array.size() * seperation
+	add_child(new_link)
+	link_array.append(new_link)
+	attach_joint()
+	
+	
+func attach_joint():
+	var new_link = link_array.back()
+	if link_array.size() > 1:
+		var new_joint := PinJoint3D.new()
+		new_joint.set_param(PinJoint3D.Param.PARAM_BIAS, 0.3)
+		new_joint.set_param(PinJoint3D.Param.PARAM_DAMPING, 1)
+		new_joint.set_param(PinJoint3D.Param.PARAM_IMPULSE_CLAMP, 0)
+		var sep = 2 * (link_array.size()-2) + 1
+		new_joint.position = link_array.front().transform.basis.y * sep
+		new_joint.node_a = new_link.get_path()
+		new_joint.node_b = link_array[link_array.size() - 2].get_path()
+		add_child(new_joint)
+		joint_array.append(new_joint)
